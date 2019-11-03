b47792944a9998c153d0111c373b644fde59ee2a
Vyacheslav <igoreshapl@gmail.com>
2019-11-03 22:30:00 +0500

    oap lab 15

---
+++ OAP/lab15/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        delegate string AddPrefix(string text); // Первый вариант реализации
+
+        delegate string AddPrefixV2(string text); // Второй вариант реализации
+        static void Main(string[] args)
+        { 
+            // Первый вариант реализации
+            AddPrefix ap = delegate (string text)
+            {
+                return "ПР " + text;
+            };
+
+            // Второй вариант реализации (лямбда-выажение)
+            AddPrefixV2 ap2 = x => "ПР " + x;
+
+            Console.Write("Введите что-то: ");
+            Console.WriteLine(ap(Console.ReadLine()));
+
+            Console.Write("Введите что-то (второй вариант): ");
+            Console.WriteLine(ap2(Console.ReadLine()));
+
+            Console.Read();
+        }
+    }
+}
